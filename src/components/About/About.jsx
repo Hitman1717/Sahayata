4e22da2f3673bb8cf1980c50e48afebedde36433
Hitman1717 <hitmanthegreat17@gmail.com>
2025-07-31 22:53:12 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 
                 <div id="what_we_Do" className="bg-white py-12 px-4 sm:px-6 lg:px-8">
-                <h1 className="text-4xl font-extrabold text-gray-800 mb-6 text-center">OUR MISSION</h1>
+                <h1 className="text-4xl font-extrabold text-gray-800 mb-6 text-center">OUR AIM</h1>
                 <p className="text-lg text-gray-700 leading-7 max-w-4xl mx-auto">
                 At Sahayata, our mission is to make a meaningful impact in the lives of those in need. We believe in the power of community and compassion, and we strive to create a world where everyone has the opportunity to thrive. Through our dedicated efforts, we aim to address pressing social issues and uplift marginalized individuals and communities. <br /> Our commitment extends beyond immediate assistance; we focus on sustainable solutions that empower individuals to improve their circumstances. By fostering collaboration among various stakeholders, we seek to create a network of support that not only meets immediate needs but also inspires long-term growth and self-sufficiency.
                 </p>
